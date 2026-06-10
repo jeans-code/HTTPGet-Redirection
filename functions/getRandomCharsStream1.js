7cb90f1a31460dbb07435cdbe70bb6a7306443d4
jeans-code <jeanunder6@gmail.com>
2026-06-10 12:10:56 -0500

    Fix Streaming

--- functions/getRandomCharsStream1.js
+++ functions/getRandomCharsStream1.js
@@ -1,11 +1,14 @@
 //const process = require("node:process");
+const { stream } = require("@netlify/functions");
+
+// https://docs.netlify.com/build/functions/lambda-compatibility/?data-tab=JavaScript
 
 let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 function getRandomChar() {
   return chars[Math.floor(Math.random() * chars.length)];
 }
 
-exports.handler = async function(event, context) {
+exports.handler = stream(async function(event, context) {
   const ip = event.headers["x-forwarded-for"];
   console.log(ip + " wants 20 random characters.");
   const encoder = new TextEncoder();
@@ -27,7 +30,15 @@ exports.handler = async function(event, context) {
     }
   });
 
-  return new Response(body);
+  // return new Response(body);
+  return {
+    headers: {
+      "content-type": "text/plain"
+    },
+    statusCode: 200,
+    body: body
+  };
 
   
-};
+// };
+});
